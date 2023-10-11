444d298f56ae61e7b4e36d2bc70de6f8b7b6f0c4
Gabrielwildan <gabrielwildan7@gmail.com>
2023-10-11 14:05:52 +0700

    membuat frontpage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Dashboard from "./pages/admin/Dashboard";
 import DataDokter from "./pages/admin/DataDokter";
 import Antrian from "./pages/admin/Antrian";
 import Login from "./pages/admin/Login";
+import Landing from "./pages/frontend/Landing";
+import Poliklinik from "./pages/frontend/Poliklinik";
+import Pendaftaran from "./pages/frontend/Pendaftaran";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Frontend Routing Start */}
+        <Route path="/welcome" element={<Landing />} />
+        <Route path="/poliklinik" element={<Poliklinik />} />
+        <Route path="/pendaftaran" element={<Pendaftaran />} />
+        {/* Frontend Routing End */}
+
+        {/* Admin Routing Start */}
         <Route path="admin/dashboard" element={<Dashboard />} />
         <Route path="admin/data-dokter" element={<DataDokter />} />
         <Route path="admin/antrian" element={<Antrian />} />
         <Route path="admin/login" element={<Login />} />
+        {/* Admin Routing End */}
       </Routes>
     </Router>
   );
